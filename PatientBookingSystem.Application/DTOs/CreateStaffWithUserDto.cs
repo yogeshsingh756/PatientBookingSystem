44534f4879609ebe0bd34239887a532e6cd876ab
yogeshsingh756 <ythakursingh98@gmail.com>
2026-04-22 11:50:00 +0530

    fix

--- PatientBookingSystem.Application/DTOs/CreateStaffWithUserDto.cs
+++ PatientBookingSystem.Application/DTOs/CreateStaffWithUserDto.cs
@@ -9,14 +9,14 @@ namespace PatientBookingSystem.Application.DTOs
     {
         // 🔹 USER FIELDS
         public string Name { get; set; }
-        public string Email { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
 
-        public string Address { get; set; } = string.Empty;
-        public string Landmark { get; set; } = string.Empty;
-        public string HouseNumber { get; set; } = string.Empty;
-        public string PinCode { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public string? Landmark { get; set; } = string.Empty;
+        public string? HouseNumber { get; set; } = string.Empty;
+        public string? PinCode { get; set; } = string.Empty;
 
         // 🔹 STAFF FIELDS
         public int StaffType { get; set; }
