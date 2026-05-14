d06ae51a13ae9d9479efe3d462b26b30a5014898
yogeshsingh756 <ythakursingh98@gmail.com>
2026-05-14 15:02:32 +0530

    fix

--- PatientBookingSystem.API/Program.cs
+++ PatientBookingSystem.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using PatientBookingSystem.Application.DTOs.Common;
 using PatientBookingSystem.Application.Extensions;
 using PatientBookingSystem.Infrastructure.Extensions;
@@ -13,7 +14,21 @@ builder.Services.AddControllers();
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("bearer", document)] = []
+    });
+});
 
 // Custom layers
 builder.Services.AddApplication();
